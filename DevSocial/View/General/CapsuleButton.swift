477301c1a673be6fa16531194eda1326c5baa5e0
Jake Correnti <jcorrenti13@gmail.com>
2020-05-30 00:44:46 -0400

    have the basic buttons and animations functioning

---
+++ DevSocial/View/General/CapsuleButton.swift
@@ -0,0 +1,45 @@
+//
+//  CapsuleButton.swift
+//  DevSocial
+//
+//  Created by Jake Correnti on 5/30/20.
+//  Copyright © 2020 Jake Correnti. All rights reserved.
+//
+
+import UIKit
+
+class CapsuleButton: UIButton {
+	
+    // -----------------------------------------
+    // MARK: Initialization
+    // -----------------------------------------
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupUI()
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		setupUI()
+	}
+	
+    // -----------------------------------------
+    // MARK: Setup UI
+    // -----------------------------------------
+	
+	private func setupUI() {
+		
+	}
+	
+	func configure(title: String, color: UIColor) {
+		setTitle(title, for: .normal)
+		setTitleColor(color, for: .normal)
+		titleLabel?.font = .boldSystemFont(ofSize: 15)
+		backgroundColor = color.withAlphaComponent(0.2)
+		let size = CGSize(width: frame.width, height: .infinity)
+		let estimatedSize = self.sizeThatFits(size)
+		layer.cornerRadius = estimatedSize.height / 2
+		widthAnchor.constraint(equalToConstant: estimatedSize.width + 16).isActive = true
+	}
+}
---
+++ DevSocial/View/Messaging/MyMessagesFAB.swift
@@ -0,0 +1,41 @@
+//
+//  MyMessagesFAB.swift
+//  DevSocial
+//
+//  Created by Jake Correnti on 5/29/20.
+//  Copyright © 2020 Jake Correnti. All rights reserved.
+//
+
+import UIKit
+
+class MyMessagesFAB: UIButton {
+	
+    // -----------------------------------------
+    // MARK: Initialization
+    // -----------------------------------------
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		setupUI()
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		
+		setupUI()
+	}
+	
+    // -----------------------------------------
+    // MARK: Setup UI
+    // -----------------------------------------
+	
+	private func setupUI() {
+		backgroundColor = UIColor(named: ColorNames.mainColor)
+		let symbolConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .bold, scale: .medium)
+		let image = UIImage(systemName: "plus",  withConfiguration: symbolConfig)
+		imageView?.tintColor = .white
+		image?.withTintColor(.white, renderingMode: .alwaysTemplate)
+		setImage(image, for: .normal)
+	}
+}
